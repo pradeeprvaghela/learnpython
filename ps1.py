efda26ffc7cb87fb6b0b36cb263bc7e0ff7266bf
Pradeepsinh Vaghela <121917439+pradeeprvaghela@users.noreply.github.com>
2024-07-18 13:37:00 -0400

    Delete ps1.py

--- ps1.py
+++
@@ -1,9 +0,0 @@
-try:
-    with open("1.txt") as a:
-        print(a.read())
-    with open("2.txt") as b:
-        print(b.read())
-    with open("3.txt") as c:
-        print(c.read())
-except Exception as e:
-    print(e)
